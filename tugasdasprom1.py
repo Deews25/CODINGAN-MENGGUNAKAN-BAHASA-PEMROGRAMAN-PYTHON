6573cc2ef16304f9f16a0431ad8e123b5af020dc
Dewita Dyan S <78549419+Deews25@users.noreply.github.com>
2021-02-07 20:03:10 +0700

    pertemuan 2

---
+++ tugasdasprom1.py
@@ -0,0 +1,8 @@
+print("^^^^^Program Menghitung Luas Segitiga^^^^^")
+a=float(input("Masukkan Alas : "))
+t=float(input("Masukkan Tinggi : "))
+luas=0.5*a*t
+print("---------------------------------------")
+print("alas :", a)
+print("tinggi :", t)
+print("maka luas segitiga adalah : " + str(luas))
