7897fb73bdac715b6e6b094c52634de79fc1eb61
Dewita Dyan S <78549419+Deews25@users.noreply.github.com>
2021-02-07 20:02:03 +0700

    Pertemuan 1

---
+++ CobaOperatorAritmatik.py
@@ -0,0 +1,32 @@
+print("-------------------------------------------")
+print("\t^^^PROGRAM KALKULATOR SEDERHANA^^^")
+print("----------------------------")
+n1=float(input("Masukkan Nilai 1 : "))
+n2=float(input("Masukkan Nilai 2 : "))
+tambah=n1+n2
+kurang=n1-n2
+kali=n1*n2
+bagi=n1/n2
+sisabagi=n1%n2
+pangkat=n1**n2
+pembagianbulat=n1//n2
+print("Hasil Nilai 1 + Nilai 2 = %.2f"%tambah)
+print("Hasil Nilai 1 - Nilai 2 = %.2f"%kurang)
+print("Hasil Nilai 1 * Nilai 2 = %.2f"%kali)
+print("Hasil Nilai 1 / Nilai 2 = %.2f"%bagi)
+print("Hasil Nilai 1 %% Nilai 2 = %.2f"%sisabagi)
+print("Hasil Nilai 1 ** Nilai 2 = %.2f"%pangkat)
+print("Hasil Nilai 1 // Nilai 2 = %.2f"%pembagianbulat)
+print('-----------------------------------')
+
+
+print("10%2:"
+
+for i in range(11):
+    print(i+2)
+
+
+
+
+
+
